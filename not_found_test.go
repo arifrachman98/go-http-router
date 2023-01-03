afac680d41a27949d013d306efef2bd41cc5b911
Arif Rachman Hakim <arifrachman149@gmail.com>
2023-01-03 17:55:27 +0700

    Update not_found_test.go

--- not_found_test.go
+++ not_found_test.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNotFound(t *testing.T) {
+	router := httprouter.New()
+	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Not Founded")
+	})
+
+	request := httptest.NewRequest("GET", "http://localhost:"+Port+"/", nil)
+	record := httptest.NewRecorder()
+
+	router.ServeHTTP(record, request)
+
+	response := record.Result()
+	body, err := io.ReadAll(response.Body)
+	errHandler(err)
+
+	assert.Equal(t, "Not Founded", string(body))
+}

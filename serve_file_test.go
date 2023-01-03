faeadaee0a10d98c30346f861ad914889430a484
Arif Rachman Hakim <arifrachman149@gmail.com>
2023-01-03 17:33:38 +0700

    Update serve_file_test.go

--- serve_file_test.go
+++ serve_file_test.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"embed"
+	"io"
+	"io/fs"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+)
+
+//go:embed resources
+var resources embed.FS
+
+func TestServeFile(t *testing.T) {
+	router := httprouter.New()
+	dir, err := fs.Sub(resources, "resources")
+	errHandler(err)
+	router.ServeFiles("/files/*filepath", http.FS(dir))
+
+	request := httptest.NewRequest("GET", "http://localhost:"+Port+"/files/hello.txt", nil)
+	record := httptest.NewRecorder()
+
+	router.ServeHTTP(record, request)
+
+	response := record.Result()
+	body, err := io.ReadAll(response.Body)
+	errHandler(err)
+
+	assert.Equal(t, "Hello HttpRouter", string(body))
+}

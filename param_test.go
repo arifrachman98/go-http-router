970a054fbcae4e6d06bf626599ef3c23c70d7607
Arif Rachman Hakim <arifrachman149@gmail.com>
2023-01-03 15:22:19 +0700

    Update param_test.go

--- param_test.go
+++ param_test.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParam(t *testing.T) {
+	router := httprouter.New()
+	router.GET("/products/:id", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		id := p.ByName("id")
+		text := "Product " + id
+		fmt.Fprint(w, text)
+	})
+
+	request := httptest.NewRequest("GET", "http://localhost:"+Port+"/products/1", nil)
+	record := httptest.NewRecorder()
+
+	router.ServeHTTP(record, request)
+
+	response := record.Result()
+	body, err := io.ReadAll(response.Body)
+	errHandler(err)
+
+	assert.Equal(t, "Product 1", string(body))
+}

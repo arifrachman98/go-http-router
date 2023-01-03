46e2f58a52ea351cf108751b9d010be9a8710cef
Arif Rachman Hakim <arifrachman149@gmail.com>
2023-01-03 18:20:41 +0700

    Update middleware_test.go

--- middleware_test.go
+++ middleware_test.go
@@ -1,1 +1,41 @@
 package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+)
+
+type LogMiddleware struct {
+	http.Handler
+}
+
+func (middleware *LogMiddleware) serveHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Recieve Request")
+	middleware.Handler.ServeHTTP(w, r)
+}
+
+func TestMiddleware(t *testing.T) {
+	router := httprouter.New()
+	router.GET("/", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		fmt.Fprint(w, "Middleware Execute")
+	})
+
+	middleware := LogMiddleware{router}
+
+	req := httptest.NewRequest("GET", "http://localhost:"+Port+"/", nil)
+	rec := httptest.NewRecorder()
+
+	middleware.serveHTTP(rec, req)
+
+	resp := rec.Result()
+	body, err := io.ReadAll(resp.Body)
+	errHandler(err)
+
+	assert.Equal(t, "Middleware Execute", string(body))
+}

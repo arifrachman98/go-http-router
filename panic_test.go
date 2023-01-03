c7dcbd17b6385dadd1868bd2e2f6544987338ca3
Arif Rachman Hakim <arifrachman149@gmail.com>
2023-01-03 17:47:20 +0700

    Update panic_test.go

--- panic_test.go
+++ panic_test.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPanicHandler(t *testing.T) {
+	router := httprouter.New()
+	router.PanicHandler = func(w http.ResponseWriter, r *http.Request, i interface{}) {
+		fmt.Fprint(w, "Panic : ", i)
+	}
+	router.GET("/", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		panic("Terjadi Error")
+	})
+
+	request := httptest.NewRequest("GET", "http://localhost:"+Port+"/", nil)
+	record := httptest.NewRecorder()
+
+	router.ServeHTTP(record, request)
+
+	response := record.Result()
+	body, err := io.ReadAll(response.Body)
+	errHandler(err)
+
+	assert.Equal(t, "Panic : Terjadi Error", string(body))
+}

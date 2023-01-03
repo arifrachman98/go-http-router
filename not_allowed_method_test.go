7cab9c748dd7ad50168737eb5f498050952fcaea
Arif Rachman Hakim <arifrachman149@gmail.com>
2023-01-03 18:07:32 +0700

    Update not_allowed_method_test.go

--- not_allowed_method_test.go
+++ not_allowed_method_test.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNotAllowedMethod(t *testing.T) {
+	router := httprouter.New()
+	router.MethodNotAllowed = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Access Denied")
+	})
+	router.POST("/", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		fmt.Fprint(w, "POST")
+	})
+
+	request := httptest.NewRequest("GET", "http://localhost:"+Port+"/", nil)
+	record := httptest.NewRecorder()
+
+	router.ServeHTTP(record, request)
+
+	response := record.Result()
+	body, err := io.ReadAll(response.Body)
+	errHandler(err)
+
+	assert.Equal(t, "Access Denied", string(body))
+}
